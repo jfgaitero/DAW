95a56e97821630336ce901ce1927d04e8eb46a08
Julio Gaitero <78016512+jfgaitero@users.noreply.github.com>
2021-02-12 12:55:43 +0100

    Create 02_ValoresVariables.java

---
+++ Programacion I/02_ValoresVariables.java
@@ -0,0 +1,15 @@
+package valores.Variables;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       System.out.println("El valor maximo de un Byte es: " +Byte.MAX_VALUE+ " y el valor minimo es: " +Byte.MIN_VALUE);
+       System.out.println("El valor maximo de un Short es: " +Short.MAX_VALUE+ " y el valor minimo es: " +Short.MIN_VALUE);
+       System.out.println("El valor maximo de un Int es: " +Integer.MAX_VALUE+ " y el valor minimo es: " +Integer.MIN_VALUE);
+       System.out.println("El valor maximo de un Long es: " +Long.MAX_VALUE+ " y el valor minimo es: " +Long.MIN_VALUE);
+       System.out.println("El valor maximo de un Float es: " +Float.MAX_VALUE+ " y el valor minimo es: " +Float.MIN_VALUE);
+       System.out.println("El valor maximo de un Double es: " +Double.MAX_VALUE+ " y el valor minimo es: " +Double.MIN_VALUE);
+
+    }
+}
